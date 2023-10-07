99f79f31908465cfd4383693784c42973c7af406
MrrChanhAYTN <nguyenthiennam.npk@gmail.com>
2023-10-07 08:37:01 +0700

    git jenkins first push

--- src/main/java/com/fev/gitJenkins/App.java
+++ src/main/java/com/fev/gitJenkins/App.java
@@ -25,35 +25,6 @@ public class App {
         js.executeScript("arguments[0].value='" + "08102003" + "'", inputPass);
         btnSubmit.click();
     }
-	@Test(priority = 2)
-	public void Log() {
-    	System.setProperty("webdriver.chrome.driver", "C:/Users/LAM/Downloads/chromedriver-win32/chromedriver-win32/chromedriver.exe");
-        WebDriver wdriver = new ChromeDriver();
-        JavascriptExecutor js = (JavascriptExecutor) wdriver;
-        wdriver.get("http://127.0.0.1:5500/login.html");
-        
-        WebElement inputName = wdriver.findElement(By.id("inputName"));
-        WebElement inputPass = wdriver.findElement(By.id("inputPass"));
-        WebElement btnSubmit = wdriver.findElement(By.id("btnSubmit"));
-        
-        js.executeScript("arguments[0].value='" + "ADMINADMIN" + "'", inputName);
-        js.executeScript("arguments[0].value='" + "08102003" + "'", inputPass);
-        btnSubmit.click();
-    }
-	@Test(priority = 3)
-	public void Logi() {
-    	System.setProperty("webdriver.chrome.driver", "C:/Users/LAM/Downloads/chromedriver-win32/chromedriver-win32/chromedriver.exe");
-        WebDriver wdriver = new ChromeDriver();
-        JavascriptExecutor js = (JavascriptExecutor) wdriver;
-        wdriver.get("http://127.0.0.1:5500/login.html");
-        
-        WebElement inputName = wdriver.findElement(By.id("inputName"));
-        WebElement inputPass = wdriver.findElement(By.id("inputPass"));
-        WebElement btnSubmit = wdriver.findElement(By.id("btnSubmit"));
-        
-        js.executeScript("arguments[0].value='" + "ADMINADMIN" + "'", inputName);
-        js.executeScript("arguments[0].value='" + "08102003" + "'", inputPass);
-        btnSubmit.click();
-    }
+	
 }
 
