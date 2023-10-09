85f30f1bc88907cc4305b092a4f50fc5ccd56d8a
MrrChanhAYTN <nguyenthiennam.npk@gmail.com>
2023-10-10 03:00:17 +0700

    First push

--- src/main/java/com/fev/gitJenkins/App.java
+++ src/main/java/com/fev/gitJenkins/App.java
@@ -1,7 +1,12 @@
 package com.fev.gitJenkins;
 
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.Cookie;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -12,16 +17,45 @@ public class App {
 
 	@Test(priority = 1)
 	public void Login() {
-    	System.setProperty("webdriver.chrome.driver", "C:/Users/LAM/Downloads/chromedriver-win32/chromedriver-win32/chromedriver.exe");
-        WebDriver wdriver = new ChromeDriver();
-        JavascriptExecutor js = (JavascriptExecutor) wdriver;
-        wdriver.get("http://127.0.0.1:5500/login.html");
-        WebElement inputName = wdriver.findElement(By.id("inputName"));
-        WebElement inputPass = wdriver.findElement(By.id("inputPass"));
-        WebElement btnSubmit = wdriver.findElement(By.id("btnSubmit"));
+		System.setProperty("webdriver.chrome.driver", "C:/Users/LAM/Downloads/chromedriver-win32/chromedriver-win32/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        driver.get("http://127.0.0.1:5500/login.html");
+        
+        WebElement inputName = driver.findElement(By.id("inputName"));
+        WebElement inputPass = driver.findElement(By.id("inputPass"));
+        WebElement btnSubmit = driver.findElement(By.id("btnSubmit"));
+        
         js.executeScript("arguments[0].value='" + "ADMINADMIN" + "'", inputName);
         js.executeScript("arguments[0].value='" + "08102003" + "'", inputPass);
-        btnSubmit.click();
+        btnSubmit.click();				
+	        		
+	        // create file named Cookies to store Login Information		
+	        File file = new File("Cookies.data");							
+	        try		
+	        {	  
+	            // Delete old file if exists
+				file.delete();		
+	            file.createNewFile();			
+	            FileWriter fileWrite = new FileWriter(file);							
+	            BufferedWriter Bwrite = new BufferedWriter(fileWrite);							
+	            // loop for getting the cookie information 		
+	            	
+	            // loop for getting the cookie information 		
+	            for(Cookie ck : driver.manage().getCookies())							
+	            {			
+	            	System.out.println(ck.getName());
+	                Bwrite.write((ck.getName()+";"+ck.getValue()+";"+ck.getDomain()+";"+ck.getPath()+";"+ck.getExpiry()+";"+ck.isSecure()));																									
+	                Bwrite.newLine();             
+	            }			
+	            Bwrite.close();			
+	            fileWrite.close();	
+	            
+	        }
+	        catch(Exception ex)					
+	        {		
+	            ex.printStackTrace();			
+	        }		
     }
 	
 }
